643c41d6c116182012e1b61e88ed13a38e009df3
Laureano Enrique <laureanoenrique29@gmail.com>
2026-02-21 14:36:42 -0300

    fix merge y mp

---
+++ core/migrations/0018_merge_20260221_0001.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0016_add_turnos_en_vivo_habilitados'),
+        ('core', '0017_fix_turnofijo_duracion_column'),
+    ]
+
+    operations = []
